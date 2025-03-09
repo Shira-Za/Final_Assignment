4f230b0735a583ae1ff718c3abfa6b164fa726ba
Shira Zadok <shirazadok@mail.tau.ac.il>
2025-03-09 13:36:29 +0200

    Uploading the final assignment script with the reason for choosing this dataset

---
+++ Final_Assignment.R
@@ -0,0 +1,19 @@
+# Final Assignment R Beginners Course
+# Shira Zadok, ID 318958311
+
+#### Part 1 - Choosing the Data : ----
+# This data set is the gathered data from an experiment conducted in our lab. 
+# This experiment examines the “Gaze Aversion” phenomena and whether people grasp 
+# other peoples’ gaze aversion as a thinking process. To manipulate gaze aversion, 
+# we asked participants to look at 9 different locations on the screen and filmed 
+# their response. We used these videos in three experiments - in one, the videos
+# were shown without changes (we only clipped them to be a few seconds long), 
+# in the second, the eyes were cut out, and only the face was shown, and in the 
+# third, only the eyes were shown. These participants were the interviewees group. 
+# Then, we showed another group of participants the videos and asked them to answer 
+# a few questions about the short videos they observed - they were the observers group. 
+# So, I chose this data set to assist a colleague with the analysis of the data 
+# and also because I find this project interesting and relevant to understanding 
+# social interactions better. 
+
+
