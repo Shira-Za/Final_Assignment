e986bc017613073c31407d59e57f20b46765efd5
Shira Zadok <shirazadok@mail.tau.ac.il>
2025-03-09 13:46:41 +0200

    Exploring the raw data with some visual graphs.

--- Final_Assignment.R
+++ Final_Assignment.R
@@ -35,3 +35,45 @@ df$AQ_Par <- factor(df$AQ_Par, levels = c(0, 1), labels = c("Low", "High"))
 df$AQ_intvwee <- factor(df$AQ_intvwee, levels = c(0, 1), labels = c("Low", "High"))
 df$expName <- factor(df$expName)
 df$action <- factor(df$action)
+
+# Exploring the Data:
+# Response times across experiments:
+p1 = ggplot(df, aes(x = response_key.rt, fill = expName)) +
+  geom_density(alpha = 0.3) +
+  theme_minimal() +
+  labs(title = "RT Distribution by Experiment", 
+       x = "Response Time (s)", y = "Density")
+
+p2 = ggplot(df, aes(x = expName, y = response_key.rt, fill = expName)) +
+  geom_boxplot() +
+  theme_minimal() +
+  labs(title = "RT Across Experiments", 
+       x = "Experiment", y = "Response Time (s)")
+
+# Accuracy Across Experiments
+p3 = ggplot(df, aes(x = expName, y = response_key.corr, fill = expName)) +
+  stat_gradientinterval() +
+  stat_summary(fun = mean, geom = "point", color = "black", size = 3) +
+  labs(title = "Accuracy by Experimental Condition", y = "Accuracy", x = "Condition") +
+  theme_minimal()
+
+# Response Time vs Accuracy
+p4 = ggplot(df, aes(x = response_key.rt, y = response_key.corr, color = expName)) +
+  geom_jitter(position = position_jitter(width = 0.2, height = 0), size = 1.5, alpha = 0.6) +
+  theme_minimal() +
+  labs(title = "Response Time vs Accuracy", x = "Response Time (s)", y = "Correct Response")
+
+# Reaction Time by AQ_Par
+p5 = ggplot(df, aes(x = AQ_Par, y = response_key.rt, fill = AQ_Par)) +
+  stat_halfeye(alpha = 0.6) +
+  labs(title = "Reaction Time by Participant AQ", y = "Reaction Time (s)", x = "AQ Group") +
+  theme_minimal()
+
+# Accuracy by AQ_Par
+p6 = ggplot(df, aes(x = AQ_Par, y = response_key.corr, fill = AQ_Par)) +
+  stat_halfeye() +
+  labs(title = "Accuracy by Participant AQ", y = "Accuracy", x = "AQ Group") +
+  theme_minimal()
+
+final_p = (p1 | p2) / (p3 | p4) / (p5 | p6)
+print(final_p)
